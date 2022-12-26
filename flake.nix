6208465eb93802c9973c4080f90f2e57d0eff901
usertam <heiyiutam@gmail.com>
2022-12-27 03:55:09 +0800

    flake.nix: cover platforms.unix

--- flake.nix
+++ flake.nix
@@ -9,16 +9,8 @@
     modules.flake = false;
   };
 
-  outputs = { self, ... }@inputs:
-    let
-      supportedSystems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "i686-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+  outputs = { self, ... }@inputs: let
+      forAllSystems = with inputs.nixpkgs.lib; genAttrs platforms.unix;
     in {
       packages = forAllSystems (system: let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
