f32f9f4671a268f859c3a85d68897631b44f9937
usertam <heiyiutam@gmail.com>
2022-12-30 04:26:39 +0800

    luatex/default.nix: simplify build

--- pkgs/luatex/default.nix
+++ pkgs/luatex/default.nix
@@ -2,13 +2,9 @@
 , src
 , runCommand
 , pkg-config
-, cairo
-, gmp
 , graphite2
 , harfbuzz
 , libpng
-, mpfr
-, pixman
 , zlib
 , zziplib
 }:
@@ -26,13 +22,9 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
-    cairo
-    gmp
     graphite2
     harfbuzz
     libpng
-    mpfr
-    pixman
     zlib
     zziplib
   ];
@@ -41,57 +33,23 @@ stdenv.mkDerivation {
 
   configureFlags = [
     "--enable-build-in-source-tree"
-    "--enable-compiler-warnings=yes"
     "--enable-silent-rules"
     "--disable-all-pkgs"
-    "--disable-shared"
-    "--disable-ptex"
-    "--disable-largefile"
-    "--disable-xetex"
-    "--disable-ipc"
-    "--disable-dump-share"
     "--disable-native-texlive-build"
-    "--enable-coremp"
-    "--enable-web2c"
     "--enable-luatex"
-    "--enable-luajittex=no"
-    "--enable-mfluajit=no"
-    "--enable-luahbtex=no"
-    "--enable-luajithbtex=no"
-    "--enable-mfluajit=no"
-    "--without-mf-x-toolkit"
     "--without-x"
-  ] ++ map (what: "--with-system-${what}") [
-    "cairo"
-    "gmp"
+  ] ++ map (lib: "--with-system-${lib}") [
     "graphite2"
     "harfbuzz"
     "libpng"
-    "mpfr"
-    "pixman"
     "zlib"
     "zziplib"
-  ] ++ map (what: "--without-system-${what}") [
-    "kpathsea"
-    "ptexenc"
-    "teckit"
-    "xpdf"
   ];
-
-  postConfigure = toString (map (dir: ''
-    local flagsArray=($configureFlags "''${configureFlagsArray[@]}")
-    ( cd ${dir} && ./configure ''${flagsArray[@]} )
-    unset flagsArray
-  '') [
-    "texk/kpathsea"
-    "texk/web2c"
-  ]);
 
   buildPhase = ''
     local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL=$SHELL $makeFlags "''${makeFlagsArray[@]}" $buildFlags "''${buildFlagsArray[@]}")
-    make ''${flagsArray[@]} -C libs
-    make ''${flagsArray[@]} -C utils
-    make ''${flagsArray[@]} -C texk/kpathsea
+    make ''${flagsArray[@]}
+    make ''${flagsArray[@]} -C libs/pplib
     make ''${flagsArray[@]} -C texk/web2c luatex
     unset flagsArray
   '';
