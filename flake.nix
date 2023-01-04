a6a3cb002f77d3e593b8b0ec89ff88db2095050e
usertam <heiyiutam@gmail.com>
2023-01-05 01:52:10 +0800

    flake.nix: format code with nixfmt

--- flake.nix
+++ flake.nix
@@ -11,36 +11,37 @@
     modules.flake = false;
   };
 
-  outputs = { self, ... }@inputs: let
-      forAllSystems = with inputs.nixpkgs.lib; genAttrs platforms.unix;
+  outputs = { self, ... }@inputs:
+    let forAllSystems = with inputs.nixpkgs.lib; genAttrs platforms.unix;
     in {
-      packages = forAllSystems (system: let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in rec {
-        luametatex = pkgs.callPackage ./pkgs/luametatex {
-          src = "${inputs.context}/source/luametatex";
-        };
-        luatex = pkgs.callPackage ./pkgs/luatex {
-          src = inputs.luatex;
-        };
-        context-minimals = pkgs.callPackage self {
-          inherit (inputs) context context-fonts modules;
-          inherit luametatex luatex;
-        };
-        default = context-minimals;
-      });
+      packages = forAllSystems (system:
+        let pkgs = inputs.nixpkgs.legacyPackages.${system};
+        in rec {
+          luametatex = pkgs.callPackage ./pkgs/luametatex {
+            src = "${inputs.context}/source/luametatex";
+          };
+          luatex = pkgs.callPackage ./pkgs/luatex {
+            src = inputs.luatex;
+          };
+          context-minimals = pkgs.callPackage self {
+            inherit (inputs) context context-fonts modules;
+            inherit luametatex luatex;
+          };
+          default = context-minimals;
+        });
 
-      apps = forAllSystems (system: let
-        mkApp = bin: {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/${bin}";
-        };
-      in {
-        context = mkApp "context";
-        luametatex = mkApp "luametatex";
-        luatex = mkApp "luatex";
-        mtxrun = mkApp "mtxrun";
-        default = self.apps.${system}.context;
-      });
+      apps = forAllSystems (system:
+        let
+          mkApp = bin: {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/${bin}";
+          };
+        in {
+          context = mkApp "context";
+          luametatex = mkApp "luametatex";
+          luatex = mkApp "luatex";
+          mtxrun = mkApp "mtxrun";
+          default = self.apps.${system}.context;
+        });
     };
 }
