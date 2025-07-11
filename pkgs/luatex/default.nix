73673ecce9c1f2e82d33bc76482aab51b73b35db
usertam <code@usertam.dev>
2025-07-11 18:35:34 +0800

    luatex: refactor with make flags; dymanic link libraries

--- pkgs/luatex/default.nix
+++ pkgs/luatex/default.nix
@@ -1,20 +1,23 @@
-{ stdenv
-, src
-, pkg-config
-, graphite2
-, harfbuzz
-, libpng
-, zlib
-, zziplib
+{
+  stdenv,
+  src,
+  autoreconfHook,
+  pkg-config,
+  graphite2,
+  harfbuzz,
+  libpng,
+  zlib,
+  zziplib,
 }:
 
-stdenv.mkDerivation (attrsFinal: {
+stdenv.mkDerivation (final: {
   pname = "luatex";
   version = "1.23.3";
 
   inherit src;
 
   nativeBuildInputs = [
+    autoreconfHook
     pkg-config
   ];
 
@@ -30,34 +33,79 @@ stdenv.mkDerivation (attrsFinal: {
 
   configureFlags = [
     "--enable-build-in-source-tree"
-    "--enable-silent-rules"
-    "--disable-all-pkgs"
+    # Enable dynamic linking with system libraries.
     "--disable-native-texlive-build"
+    "--enable-shared"
+    # Disable all except luatex.
+    "--disable-all-pkgs"
     "--enable-luatex"
+    "--without-system-kpathsea"
     "--without-x"
-  ] ++ map (lib: "--with-system-${lib}") [
-    "graphite2"
-    "harfbuzz"
-    "libpng"
-    "zlib"
-    "zziplib"
-  ];
+  ] ++ map (lib: "--with-system-${lib}") (map (x: x.pname) final.buildInputs);
 
-  buildPhase = ''
-    local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL=$SHELL $makeFlags "''${makeFlagsArray[@]}" $buildFlags "''${buildFlagsArray[@]}")
-    make ''${flagsArray[@]}
-    make ''${flagsArray[@]} -C libs/pplib
-    make ''${flagsArray[@]} -C texk/web2c luatex
+  # Regenerate configure scripts for implicit luatex dependencies.
+  postAutoreconf = ''
+    autoreconf "''${flagsArray[@]}" \
+      texk/web2c texk/kpathsea libs/lua53 libs/pplib
+  '';
+
+  # Ahead of the build phase, use make to configure all targets first.
+  postConfigure = ''
+    local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL="$SHELL")
+    concatTo flagsArray makeFlags makeFlagsArray
+
+    echoCmd 'post-configure flags' "''${flagsArray[@]}"
+    make "''${flagsArray[@]}"
+
     unset flagsArray
   '';
 
-  installPhase = ''
-    install -Dm555 -t $out/bin texk/web2c/luatex
+  # Before building luatex itself, build dependency pplib first.
+  preBuild = ''
+    local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL="$SHELL")
+    concatTo flagsArray makeFlags makeFlagsArray
+
+    echoCmd 'pre-build flags' "''${flagsArray[@]}" '-C libs/pplib'
+    make "''${flagsArray[@]}" -C libs/pplib
+
+    unset flagsArray
+  '';
+
+  buildFlags = [
+    "-C texk/web2c"
+    "luatex"
+  ];
+
+  installFlags = [
+    "-C texk/web2c"
+    "lib_LTLIBRARIES="
+    "bin_PROGRAMS=luatex"
+  ];
+
+  installTargets = [ "install-binPROGRAMS" ];
+
+  # After installing luatex, install library kpathsea, lua53 and texmf.cnf.
+  postInstall = ''
+    local flagsArray=(''${enableParallelBuilding:+-j''${NIX_BUILD_CORES}} SHELL="$SHELL")
+    concatTo flagsArray makeFlags makeFlagsArray
+
+    # Don't create directories.
+    substituteInPlace texk/kpathsea/Makefile \
+      --replace-warn 'install-data-local: installdirs-am' 'install-data-local:'
+
+    echoCmd 'post-install flags' "''${flagsArray[@]}" '-C texk/kpathsea install-libLTLIBRARIES install-data-local'
+    make "''${flagsArray[@]}" -C texk/kpathsea install-libLTLIBRARIES install-data-local
+
+    echoCmd 'post-install flags' "''${flagsArray[@]}" '-C libs/lua53 install-libLTLIBRARIES'
+    make "''${flagsArray[@]}" -C libs/lua53 install-libLTLIBRARIES
+
+    unset flagsArray
   '';
 
   passthru.computedVersion =
     let
-      source = builtins.readFile (attrsFinal.src + "/texk/web2c/luatexdir/luatex.c");
+      source = builtins.readFile (final.src + "/texk/web2c/luatexdir/luatex.c");
       versionMatch = builtins.match ''.*[^a-z0-9_]luatex_version_string[ \t]*=[ \t]*"([^"]*)";.*'' source;
-    in builtins.elemAt versionMatch 0;
+    in
+    builtins.elemAt versionMatch 0;
 })
